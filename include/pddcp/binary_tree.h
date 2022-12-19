7d71c39b100fd7e3d3b29f0ab87a2eebe79f2250
Derek Huang <djh458@stern.nyu.edu>
2022-12-18 19:03:34 -0500

    fix binary_tree.h errors + add and pass tests
    CMakeLists.txt
        Suppress GCC -Wparentheses warning
    
    include/pddcp/binary_tree.h
        Fix syntax errors
    
    test/CMakeLists.txt
        Separate standalone tests from library unit tests using comment

--- include/pddcp/binary_tree.h
+++ include/pddcp/binary_tree.h
@@ -9,6 +9,7 @@
 #define PDDCP_BINARY_TREE_H_
 
 #include <memory>
+#include <optional>
 #include <vector>
 
 namespace pddcp {
@@ -22,6 +23,7 @@ template <typename T>
 class binary_tree {
 public:
   using value_type = T;
+  using self_type = binary_tree<T>;
 
   /**
    * Create binary tree node without children.
@@ -36,24 +38,24 @@ public:
    * The left and right children must not be owned by other smart pointers.
    *
    * @param value node value
-   * @param left `binary_tree*` left child to take ownership of
-   * @param right `binary_tree*` right child to take ownership of
+   * @param left `self_type*` left child to take ownership of
+   * @param right `self_type*` right child to take ownership of
    */
-  binary_tree(T value, binary_tree* left, binary_tree* right)
+  binary_tree(T value, self_type* left, self_type* right)
     : value_(value), left_(left), right_(right)
   {}
 
   // getters, including convenience getters for left + right values
   const T& value() const noexcept { return value_; }
-  binary_tree* left() const noexcept { return left_.get(); }
+  self_type* left() const noexcept { return left_.get(); }
   const T& left_value() const { return left_->value_; }
-  binary_tree* right() const noexcept { return right_.get(); }
+  self_type* right() const noexcept { return right_.get(); }
   const T& right_value() const { return right_->value_; }
 
   /**
    * Update the node value.
    *
-   * @param value `T` new node value
+   * @param new_value `T` new node value
    * @returns the previous node value
    */
   auto set_value(T new_value)
@@ -66,33 +68,69 @@ public:
   /**
    * Update the left subtree by taking ownership of a node pointer.
    *
-   * @param new_left `binary_tree*` to take ownership of as left subtree
-   * @returns `std::unique_ptr<binary_tree>` owning old left subtree
+   * @param new_left `self_type*` to take ownership of as left subtree
+   * @returns `std::unique_ptr<self_type>` owning old left subtree
    */
-  auto set_left(binary_tree* new_left)
+  auto set_left(self_type* new_left)
   {
-    auto old_left = std::make_unique<binary_tree>(left_.release());
+    std::unique_ptr<self_type> old_left{left_.release()};
     left_.reset(new_left);
     return old_left;
   }
 
   /**
+   * Update the value of the left child.
+   *
+   * If there is no left child, it will be created.
+   *
+   * @param new_value `T` new left node value
+   * @returns `std::optional<T>` containing previous left node value. If there
+   *  was no previous left node, then the `std::optional<T>` is empty.
+   */
+  std::optional<T> set_left_value(T new_value)
+  {
+    if (!left_.get()) {
+      left_.reset(new binary_tree{new_value});
+      return {};
+    }
+    return left_->set_value(new_value);
+  }
+
+  /**
    * Update the right subtree by taking ownership of a node pointer.
    *
-   * @param new_right `binary_tree*` to take ownership of as right subtree
-   * @returns `std::unique_ptr<binary_tree>` owning old right subtree
+   * @param new_right `self_type*` to take ownership of as right subtree
+   * @returns `std::unique_ptr<self_type>` owning old right subtree
    */
-  auto set_right(binary_tree* new_right)
+  auto set_right(self_type* new_right)
   {
-    auto old_right = std::make_unique<binary_tree>(right_.release());
+    std::unique_ptr<self_type> old_right{right_.release()};
     right_.reset(new_right);
     return old_right;
   }
 
+    /**
+   * Update the value of the right child.
+   *
+   * If there is no right child, it will be created.
+   *
+   * @param new_value `T` new right node value
+   * @returns `std::optional<T>` containing previous right node value. If there
+   *  was no previous right node, then the `std::optional<T>` is empty.
+   */
+  std::optional<T> set_right_value(T new_value)
+  {
+    if (!right_.get()) {
+      right_.reset(new binary_tree{new_value});
+      return {};
+    }
+    return right_->set_value(new_value);
+  }
+
 private:
   T value_;
-  std::unique_ptr<binary_tree> left_;
-  std::unique_ptr<binary_tree> right_;
+  std::unique_ptr<self_type> left_;
+  std::unique_ptr<self_type> right_;
 };
 
 namespace bst {
@@ -110,19 +148,23 @@ namespace bst {
 template <typename T>
 auto insert(binary_tree<T>* root, T value)
 {
+  // note: for both left and right inserts, we could use single-line if-else,
+  // but we want to allow auto deduction and so have explicit returns
   // left insert
   if (value < root->value()) {
-    if (!root->left())
+    if (!root->left()) {
       root->set_left(new binary_tree(value));
-    else
-      insert(root->left(), value);
+      return root;
+    }
+    insert(root->left(), value);
     return root;
   }
   // right insert
-  if (!root->right())
+  if (!root->right()) {
     root->set_right(new binary_tree(value));
-  else
-    insert(root->right(), value);
+    return root;
+  }
+  insert(root->right(), value);
   return root;
 }
 
@@ -152,24 +194,24 @@ inline auto insert(binary_tree<T>* root, const std::vector<T>& values)
  * @param root root of binary tree to verify
  */
 template <typename T>
-bool check(const binary_tree<T>* root)
+auto check(const binary_tree<T>* root)
 {
   // false on NULL
   if (!root)
     return false;
   // check left and right values
   if (
-    root.left() && root.left_value() >= root.value() ||
-    root.right() && root.right_value() < root.value()
+    root->left() && root->left_value() >= root->value() ||
+    root->right() && root->right_value() < root->value()
   )
     return false;
   // check recursively, although if subtree is NULL, treat as true
-  auto left_ok = (root.left()) ? is_bst(root.left()) : true;
+  auto left_ok = (root->left()) ? check(root->left()) : true;
   // if left is not ok, we can return false earlier before evaluting right
   if (!left_ok)
     return false;
   // otherwise, left is ok, only need to check right
-  auto right_ok = (root.right()) ? is_bst(root.right()) : true;
+  auto right_ok = (root->right()) ? check(root->right()) : true;
   if (!right_ok)
     return false;
   return true;
