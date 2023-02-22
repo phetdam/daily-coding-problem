3fbd72c3ca173824472fcebd7b56204199330fe2
Derek Huang <djh458@stern.nyu.edu>
2023-02-21 21:00:44 -0500

    AddGTestStandalone.cmake: use PRIVATE linking + add custom main standalone

--- cmake/AddGTestStandalone.cmake
+++ cmake/AddGTestStandalone.cmake
@@ -8,5 +8,16 @@ cmake_minimum_required(VERSION ${CMAKE_MINIMUM_REQUIRED_VERSION})
 #
 function(pdccp_add_gtest_standalone target_name)
     add_executable(${target_name} ${target_name}.cc)
-    target_link_libraries(${target_name} GTest::gtest_main)
+    target_link_libraries(${target_name} PRIVATE GTest::gtest_main)
+endfunction()
+
+##
+# Add a Google Test runner with custom main from a single C++ source file.
+#
+# Arguments:
+#   target_name - Name of the output executable and C++ source file stem
+#
+function(pddcp_add_gtest_custom_main target_name)
+    add_executable(${target_name} ${target_name}.cc)
+    target_link_libraries(${target_name} PRIVATE GTest::gtest)
 endfunction()
