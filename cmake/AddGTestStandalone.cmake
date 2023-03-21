429187dda540de2634c619655a82430dcb18836e
Derek Huang <djh458@stern.nyu.edu>
2023-03-21 00:28:39 -0400

    fix CMake function name typo + update in CMakeLists.txt
    cmake/AddGTestStandalone.cmake
        Fix name to pddcp_add_gtest_standalone
    
    src/CMakeLists.txt
        Refactor using intended correct name

--- cmake/AddGTestStandalone.cmake
+++ cmake/AddGTestStandalone.cmake
@@ -6,7 +6,7 @@ cmake_minimum_required(VERSION ${CMAKE_MINIMUM_REQUIRED_VERSION})
 # Arguments:
 #   target_name - Name of the output executable and C++ source file stem
 #
-function(pdccp_add_gtest_standalone target_name)
+function(pddcp_add_gtest_standalone target_name)
     add_executable(${target_name} ${target_name}.cc)
     target_link_libraries(${target_name} PRIVATE GTest::gtest_main)
 endfunction()
