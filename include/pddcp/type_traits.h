db7ac2af9b3e129b01bf99dcf1ac2f76684e0574
Derek Huang <djh458@stern.nyu.edu>
2023-03-26 02:47:37 -0400

    type_traits.h: rewrite is_homogenous_pair with is_homogenous_tuple
    Added more general is_[leading_]homogenous_tuple with value helper and use
    is_homogenous_tuple to define is_homogenous_pair

--- include/pddcp/type_traits.h
+++ include/pddcp/type_traits.h
@@ -8,10 +8,60 @@
 #ifndef PDDCP_TYPE_TRAITS_H_
 #define PDDCP_TYPE_TRAITS_H_
 
+#include <cstdint>
 #include <tuple>
 #include <type_traits>
 
 namespace pddcp {
+
+/**
+ * Class template for checking if the first `N` types of a tuple are the same.
+ *
+ * @tparam N Number of types to check
+ * @tparam T Tuple-like type with at least `N` types
+ */
+template <std::size_t N, typename T>
+struct is_leading_homogenous_tuple : std::bool_constant<
+  is_leading_homogenous_tuple<N - 1, T>::value &&
+  std::is_same_v<
+    std::tuple_element_t<N - 1, T>, std::tuple_element_t<N - 2, T>>> {};
+
+/**
+ * Base case specialization for `is_leading_homogenous_tuple<N, T>`.
+ *
+ * @tparam T Tuple-like type with at least 1 type
+ */
+template <typename T>
+struct is_leading_homogenous_tuple<1, T> : std::true_type {};
+
+/**
+ * Helper for `is_leading_homogenous_tuple` to get the constexpr bool value.
+ *
+ * @tparam N Number of types to check
+ * @tparam T Tuple-like type with at least `N` types
+ */
+template <std::size_t N, typename T>
+inline constexpr bool
+is_leading_homogenous_tuple_v = is_leading_homogenous_tuple<N, T>::value;
+
+/**
+ * Class template for checking if all the types of a tuple are the same.
+ *
+ * @tparam N Number of types in tuple
+ * @tparam T Tuple-like type with `N` types
+ */
+template <std::size_t N, typename T>
+struct is_homogenous_tuple : std::bool_constant<
+  std::tuple_size_v<T> == N && is_leading_homogenous_tuple_v<N, T>> {};
+
+/**
+ * Helper for `is_homogenous_type` to get the constexpr bool value.
+ *
+ * @tparam N Number of types in tuple
+ * @tparam T Tuple-like type with `N` types
+ */
+template <std::size_t N, typename T>
+inline constexpr bool is_homogenous_tuple_v = is_homogenous_tuple<N, T>::value;
 
 /**
  * Class template for determining if a type is a homogenous pair.
@@ -21,9 +71,7 @@ namespace pddcp {
  * @tparam T type
  */
 template <typename T>
-struct is_homogenous_pair : std::bool_constant<
-  std::tuple_size_v<T> == 2 &&
-  std::is_same_v<std::tuple_element_t<0, T>, std::tuple_element_t<1, T>>> {};
+struct is_homogenous_pair : is_homogenous_tuple<2, T> {};
 
 /**
  * Helper for `is_homogenous_pair` to get the constexpr bool value.
