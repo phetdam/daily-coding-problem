4288b4e093b0341518805d72944ea289036d8b5c
Derek Huang <djh458@stern.nyu.edu>
2023-06-19 21:06:55 -0400

    common.h: add cross-compiler C++ version macro + helpers for C++20 conditional compilation

--- include/pddcp/common.h
+++ include/pddcp/common.h
@@ -31,4 +31,38 @@
  */
 #define PDDCP_XSTRINGIFY(p, x) PDDCP_CONCAT(p, PDDCP_STRINGIFY(x))
 
+/**
+ * C++ version compatibility macro.
+ *
+ * Allows correct C++ long integer version reporting for MSVC without needing
+ * to compile with `/Zc:__cplusplus`, as it is 199711L by default.
+ */
+#ifdef _MSC_VER
+#define PDDCP_CPLUSPLUS _MSVC_LANG
+#else
+#define PDDCP_CPLUSPLUS __cplusplus
+#endif  // _MSC_VER
+
+/**
+ * Indicate if we are compiling under C++20 or not.
+ */
+#if PDDCP_CPLUSPLUS >= 202002L
+#define PDDCP_HAS_CPP20 1
+#else
+#define PDDCP_HAS_CPP20 0
+#endif  // PDDCP_CPLUSPLUS < 202002L
+
+/**
+ * Enable some block of code when compiling with C++20.
+ *
+ * Use of variadic args is to allow chunks that contain commas.
+ *
+ * @param ... Statements to enable when compiling with C++20
+ */
+#if PDDCP_HAS_CPP20
+#define PDDCP_CPP20_ENABLE(...) __VA_ARGS__
+#else
+#define PDDCP_CPP20_ENABLE(...)
+#endif  // !PDDCP_HAS_CPP20
+
 #endif  // PDDCP_COMMON_H_
