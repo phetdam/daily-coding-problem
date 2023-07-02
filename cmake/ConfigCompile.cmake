4a123be4d6a4371ada487ca9e7bbe09ff073a6a4
Derek Huang <djh458@stern.nyu.edu>
2023-07-01 20:20:04 -0400

    ConfigCompile.cmake: add ENABLE_ASAN option to compile with AddressSanitizer

--- cmake/ConfigCompile.cmake
+++ cmake/ConfigCompile.cmake
@@ -26,6 +26,27 @@ if(MSVC)
         # /Od applied by default when using Debug configuration
         $<$<NOT:$<CONFIG:Release>>:/DEBUG>
     )
+    # enable AddressSanitizer use
+    if(ENABLE_ASAN)
+        message(STATUS "AddressSanitizer (/fsanitize=address) enabled")
+        # must remove run-time check options from C++ flags. CMake adds these
+        # by default in CMAKE_C(XX)_FLAGS_DEBUG, so remove them
+        string(
+            REGEX REPLACE
+            "/RTC[1csu]" "" CMAKE_C_FLAGS_DEBUG ${CMAKE_C_FLAGS_DEBUG}
+        )
+        message(STATUS "Removed /RTC[1csu] from CMAKE_C_FLAGS_DEBUG")
+        string(
+            REGEX REPLACE
+            "/RTC[1csu]" "" CMAKE_CXX_FLAGS_DEBUG ${CMAKE_CXX_FLAGS_DEBUG}
+        )
+        message(STATUS "Removed /RTC[1csu] from CMAKE_CXX_FLAGS_DEBUG")
+        add_compile_options(/fsanitize=address)
+        # need to disable MSVC std::vector annotation for static Google Test
+        add_compile_definitions(_DISABLE_VECTOR_ANNOTATION)
+        # AddressSanitizer cannot be used with incremental linking
+        add_link_options(/INCREMENTAL:NO)
+    endif()
 else()
     add_compile_options(
         -Wall
@@ -34,4 +55,11 @@ else()
         # -O0 is default optimization level anyways, Clang ignores -ggdb
         $<$<NOT:$<CONFIG:Release>>:-ggdb> $<IF:$<CONFIG:Release>,-O3,-O0>
     )
+    # enable AddressSanitizer use
+    if(ENABLE_ASAN)
+        message(STATUS "AddressSanitizer (-fsanitize=address) enabled")
+        # must specifiy for both compile and link
+        add_compile_options(-fsanitize=address)
+        add_link_options(-fsanitize=address)
+    endif()
 endif()
